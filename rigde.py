8cdb47914bd436a90110e3c30c3c7d82e48c4d2f
okiochan <maryalx@mail.ru>
2017-12-16 23:18:48 +0300

    uuu

--- rigde.py
+++ rigde.py
@@ -3,20 +3,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib
 
-
-def RegressionInv(X):
-    return X.T.dot(X)
-
-def LSRegression(X,y):
-    l = X.shape[0]
-    n = X.shape[1]
-    
-    ones = np.atleast_2d(np.ones(l)).T
-    X = np.concatenate((ones,X),axis=1)
-
-    res = np.linalg.inv(X.T.dot(X)).dot(X.T.dot(y))
-    return res[1:(n+1)], res[0]
-
 def GenerateSample():
     l = 50
     real = 2
@@ -40,11 +26,10 @@ n = X.shape[1]
 Cov = X.T.dot(X)
 E, V = np.linalg.eigh(Cov)
 
-# print ("\ncov matrix")
-# print(Cov)
-# print("\n\neigen values for cov matrix")
-# print(E)
-#print(V)
+print ("\ncov matrix")
+print(Cov)
+print("\n\neigen values for cov matrix")
+print(E)
 
 c = 0.001
 Cov2 = Cov + c * np.eye(n)
